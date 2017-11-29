06df52c445ef7ee6843682c8e4dedc3f961af11d
schoeu <schoeu89@gmail.com>
2017-11-29 18:16:00 +0800

    add change direction action to webhook.

--- exec/exec.go
+++ exec/exec.go
@@ -3,6 +3,7 @@ package exec
 import (
 	"os/exec"
 	"strings"
+	"path/filepath"
 )
 
 // Output is the struct that hold a command's output
@@ -12,6 +13,8 @@ type Output struct {
 	// Stderr will have error object of exec.Command
 	Stderr error
 }
+
+var curDir = ""
 
 // run will process the specified cmd, execute it and return the Output struct
 func Run(cmd string) *Output {
@@ -29,10 +32,32 @@ func processCmdStr(cmd string) (string, []string) {
 
 // execute will run the specified command with arguments
 // and return output, error
-func execute(cmd string, args ...string) ([]byte, error) {
-	out, err := exec.Command(cmd, args...).Output()
+func execute(c string, args ...string) ([]byte, error) {
+	cmd := exec.Command(c, args...)
+	preDir := curDir
+	isChangeDir := c == "cd"
+	if isChangeDir {
+		p := args[0]
+		if filepath.IsAbs(p) {
+			curDir = p
+		} else {
+			curDir = filepath.Join(curDir, p)
+		}
+		// After change direction should show the file list.
+		cmd = exec.Command("ls", args...)
+	}
+	cmd.Dir = curDir
+
+	out, err := cmd.Output()
 	if err != nil {
+		// If catch an error when change direction
+		// should rollback the path.
+		if isChangeDir {
+			curDir = preDir
+		}
+
 		return nil, err
 	}
+
 	return out, nil
 }
